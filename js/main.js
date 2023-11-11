babc429075b37a2e56f12f84d7e4700c1f97a1ac
Vutha Phim <109074461+vuthaphim0206@users.noreply.github.com>
2023-11-11 18:44:02 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Nav Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header_scrolled")
+    }else{
+        nav.classList.remove("header_scrolled")
+    }
+}
+
+
+// Hide Nav
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
